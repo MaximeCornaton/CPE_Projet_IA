56ada93997cbf34acc504650af7fe03245d06541
maximecornaton <maxime.cornaton@cpe.fr>
2023-12-04 19:00:23 +0100

    init

---
+++ utils/file_utils.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+def create_folder_if_not_exists(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        return True
+    return False
+
+def remove_folder_if_exists(folder_path):
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+        return True
+    return False
+
+def remove_files_in_folder(folder_path):
+    if os.path.exists(folder_path):
+        for file in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                print(e)
+        return True
+    return False
