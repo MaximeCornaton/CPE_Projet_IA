d26cf52bb19af7b8059edf925d9c03dd8778ab7f
maximecornaton <maxime.cornaton@cpe.fr>
2023-12-06 13:45:23 +0100

    preprocessing

--- utils/text_preprocessing.py
+++ utils/text_preprocessing.py
@@ -1,17 +1,54 @@
-def has_no_letters(line):
+def has_no_letters(line : str) -> bool:
+    """Check if line has no letters
+    Args:
+        line (str): line to check
+    Returns:
+        bool: True if line has no letters, False otherwise
+    """
     return not any(c.isalpha() for c in line)
 
-def has_no_digits(line):
+def has_no_digits(line : str) -> bool:
+    """Check if line has no digits
+    Args:
+        line (str): line to check
+    Returns:
+        bool: True if line has no digits, False otherwise
+    """
     return not any(c.isdigit() for c in line)
 
-def has_no_punctuation(line):
+def has_no_punctuation(line : str) -> bool:
+    """Check if line has no punctuation
+    Args:
+        line (str): line to check
+    Returns:
+        bool: True if line has no punctuation, False otherwise
+    """
     return not any(c in '.,;:?!' for c in line)
 
-def has_no_special_char(line):
+def has_no_special_char(line : str) -> bool:
+    """Check if line has no special characters
+    Args:
+        line (str): line to check
+    Returns:
+        bool: True if line has no special characters, False otherwise
+    """
     return not any(c in '()[]{}<>@#$%^&*_-=+|\/~`' for c in line)
 
-def is_empty_line(line):
+def is_empty_line(line : str) -> bool:
+    """Check if line is empty
+    Args:
+        line (str): line to check
+    Returns:
+        bool: True if line is empty, False otherwise
+    """
     return line == '\n'
 
-def is_too_short(line):
-    return len(line) < 5
+def is_too_short(line : str, min_length : int) -> bool:
+    """Check if line is too short
+    Args:
+        line (str): line to check
+        min_length (int): minimum length of the line
+    Returns:
+        bool: True if line is too short, False otherwise
+    """
+    return len(line) < min_length
